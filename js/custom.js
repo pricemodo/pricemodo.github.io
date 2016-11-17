cc8c9815a4a7a33f26b06be71cc80b96c2f83ab3
Young Teesy <tonyl7126@gmail.com>
2016-11-17 13:03:20 -0800

    remove delay on loading page and change loading text

--- js/custom.js
+++ js/custom.js
@@ -754,8 +754,8 @@ $(window).load(function() {
     |--------------------------------------------------------------------------
     */ 
     $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(350).css({'overflow':'visible'});
+    $('#preloader').fadeOut(); // will fade out the white DIV that covers the website.
+    $('body').css({'overflow':'visible'});
 
     /*
     |--------------------------------------------------------------------------
